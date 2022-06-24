a77734d47acbbd56de20e599cc1f31bc00fe3510
Alexandr <aleksandr.pronin.2001@gmail.com>
2022-06-24 05:11:28 +0400

    Changed to one iteration realisation

--- src/models/Streams.java
+++ src/models/Streams.java
@@ -7,9 +7,23 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+// В общем сделать так, чтобы все функции за одну итерацию по массиву выполнялись
 public class Streams<T> {
 
+    /** Main collection */
     private static List streamList;
+
+    /** List of filters
+     * Adding new filter per calling .filter()
+     * All filters using only after calling .toMap()
+     * */
+    private static List<Predicate> filtersList;
+
+    /** List of functions
+     * Adding new function per calling .transform()
+     * All functions using only after calling .toMap()
+     * */
+    private static List<Function> functionsList;
 
     public static List getStreamList() {
         return streamList;
@@ -17,41 +31,53 @@ public class Streams<T> {
 
     public static Streams of(List list){
         streamList = new LinkedList(list);
+        functionsList = new LinkedList<>();
+        filtersList = new LinkedList<>();
         return new Streams();
     }
 
     public Streams<T> filter(Predicate<? super T> predicate){
-        List result = new LinkedList();
 
-        streamList.forEach(o -> {
-            if (predicate.test((T)o)) result.add(o);
-        });
-
-        streamList = result;
+        filtersList.add(predicate);
         return this;
     }
 
     public Streams<? super T> transform(Function<? super T, ? super T> function){
 
-        List result = new LinkedList();
-
-        streamList.forEach(o -> {
-            result.add(function.apply((T)o));
-        });
-
-        streamList = result;
+        functionsList.add(function);
         return this;
     }
 
     public <K, V> Map<K, V> toMap(Function<? super T, ? extends K> keyFunction,
                            Function<? super T, ? extends V> valueFunction){
+
         Map<K, V> map = new HashMap();
 
-        streamList.forEach(o -> {
-            map.put(keyFunction.apply((T)o),
-                    valueFunction.apply((T)o));
-        });
+        boolean addToResult;
+        Object transformedObject;
 
+        for (Object obj: streamList){
+
+            addToResult = true;
+
+            for (Predicate filter: filtersList){
+                if (!filter.test(obj)) {
+                    addToResult = false;
+                    break;
+                }
+            }
+
+            if (!addToResult) continue;
+
+            transformedObject = obj;
+            for (Function function: functionsList){
+                System.out.println("Here");
+                System.out.println(obj);
+                transformedObject = function.apply((T)transformedObject);
+            }
+            map.put(keyFunction.apply((T)transformedObject),
+                    valueFunction.apply((T)transformedObject));
+        }
         return map;
     }
 
